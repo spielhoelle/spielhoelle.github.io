2b5dbd8442cb1b1d4e33f1692a432e4f20e79b2f
Tommy <spielhoelle@posteo.net>
2024-07-05 22:01:43 +0900

    feat: popup logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("yoyooyo from script.js")
+const buttonEle = document.getElementById("trigger")
+const closeButtonEle = document.getElementById("close")
+const modalEle = document.getElementById("popup")
+console.log(buttonEle)
+buttonEle.addEventListener("click", (event) => {
+	modalEle.classList.toggle("hidden")
+})
+closeButtonEle.addEventListener("click", (event) => {
+	modalEle.classList.toggle("hidden")
+})
